ebd78eb2756f56ebad51fcd97435e9eca3c64472
Maxim Suslov <suslov.mn@gmail.com>
2021-08-24 17:45:09 +0300

    Add policies for students
    - debug1
    - dyn1

--- students.tf
+++ students.tf
@@ -4,39 +4,136 @@ resource "aws_iam_group" "students" {
   # path = "/"
 }
 
-resource "aws_iam_policy" "student" {
-  name        = "${local.prefix}student-policy"
+resource "aws_iam_policy" "student_general" {
+  name        = "${local.prefix}student-general"
   path        = "/"
-  description = "Permissions for students"
+  description = "Set of general rules"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = [
-          "eks:DescribeNodegroup",
-          "eks:ListNodegroups",
-          "eks:DescribeCluster",
-          "eks:ListClusters",
-          "eks:AccessKubernetesApi",
-          "ssm:GetParameter",
-          "eks:ListUpdates",
-          "eks:ListFargateProfiles"
-        ]
-        Effect = "Allow"
-        # TODO: restrict access more
-        Resource = "*"
+        "Sid" : "DenyUnapprovedAction",
+        "Effect" : "Deny",
+        "Action" : [
+          "ds:*",
+          "iam:CreateUser",
+          "cloudtrail:StopLogging",
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "ec2:DescribeAccountAttributes",
+        "Resource" : "*"
       },
     ]
   })
   tags = merge(var.tags, {})
 }
 
-resource "aws_iam_group_policy_attachment" "users_attach_policy_StudentPolicy" {
+resource "aws_iam_policy" "student_ec2_keypairs" {
+  name        = "${local.prefix}student-ec2-keypairs"
+  path        = "/"
+  description = "Set of ec2-keypairs rules"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DescribeKeyPairs",
+          "ec2:ImportKeyPair",
+          "ec2:CreateKeyPair",
+          "ec2:DeleteKeyPair",
+        ],
+        "Resource" : "*",
+        # "Resource" : "arn:aws:ec2:eu-central-1:901850860342:*",
+        # "Resource" : "arn:aws:ec2:eu-central-1:901850860342:key-pair/*",
+      },
+      # {
+      #   "Effect" : "Allow",
+      #   "Action" : [
+      #     "iam:DeleteSSHPublicKey",
+      #     "iam:GetSSHPublicKey",
+      #     "iam:ListSSHPublicKeys",
+      #     "iam:UpdateSSHPublicKey",
+      #     "iam:UploadSSHPublicKey"
+      #   ],
+      #   "Resource" : "arn:aws:iam::*:user/$${aws:username}"
+      # },
+    ]
+  })
+  tags = merge(var.tags, {})
+}
+
+# Based on:
+# https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_examples_ec2_securitygroups-vpc.html
+resource "aws_iam_policy" "student_ec2" {
+  name        = "${local.prefix}student-ec2"
+  path        = "/"
+  description = "Set of ec2 rules"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSecurityGroupRules",
+          "ec2:DescribeTags",
+          # new rule (-s) comparing with reference above
+          "ec2:CreateTags",
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:ModifySecurityGroupRules"
+        ],
+        "Resource" : [
+          "arn:aws:ec2:eu-central-1:901850860342:security-group-rule/*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:AuthorizeSecurityGroupEgress",
+          "ec2:RevokeSecurityGroupEgress",
+          "ec2:ModifySecurityGroupRules",
+          "ec2:UpdateSecurityGroupRuleDescriptionsIngress",
+          "ec2:UpdateSecurityGroupRuleDescriptionsEgress",
+          # new rule (-s) comparing with reference above
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DescribeSecurityGroups",
+          "ec2:CreateSecurityGroup",
+          "ec2:DeleteSecurityGroup",
+        ],
+        "Resource" : "arn:aws:ec2:eu-central-1:901850860342:security-group/*",
+      },
+    ]
+  })
+  tags = merge(var.tags, {})
+}
+
+resource "aws_iam_group_policy_attachment" "users_attach_policy_StudentGeneral" {
   group      = aws_iam_group.students.name
-  policy_arn = aws_iam_policy.student.arn
+  policy_arn = aws_iam_policy.student_general.arn
+}
+
+resource "aws_iam_group_policy_attachment" "users_attach_policy_StudentEc2Keypairs" {
+  group      = aws_iam_group.students.name
+  policy_arn = aws_iam_policy.student_ec2_keypairs.arn
+}
+
+resource "aws_iam_group_policy_attachment" "users_attach_policy_StudentEc2" {
+  group      = aws_iam_group.students.name
+  policy_arn = aws_iam_policy.student_ec2.arn
 }
 
 resource "aws_iam_user" "student" {
