135ee3d39196c7979c56fd0649bed0983d1c8805
Maxim Suslov <suslov.mn@gmail.com>
2021-08-27 16:00:06 +0300

    Add s3 remote state backend

--- versions.tf
+++ versions.tf
@@ -7,6 +7,15 @@ terraform {
       version = ">= 3.51"
     }
   }
+
+  backend "s3" {
+    bucket         = "msuslov-tfstate-bucket"
+    dynamodb_table = "msuslov-tfstate-lock"
+    key            = "infra-terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+    profile        = "terraform-msuslov"
+  }
 }
 
 provider "aws" {
